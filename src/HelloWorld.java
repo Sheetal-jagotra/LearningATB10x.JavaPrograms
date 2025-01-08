9c6d2bcd011f7e458cd1edc2bc1adc1cffd527f1
Sheetal Jagotra <sheetaljagotra@Sheetals-MacBook-Air.local>
2025-01-08 23:05:08 +0530

    sum

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String agr[]){
         System.out.println("Small world ");
         System.out.println("My name is sheetal");
+        System.out.println("2+2=4");
 
     }
 }
