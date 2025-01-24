ff9426a63faec689b933e5341eadfc60066a84d6
Sheetal-jagotra <sheetal95jagotra@gmail.com>
2025-01-24 22:31:07 +0530

    input

---
+++ src/User_Input/Detail.java
@@ -0,0 +1,16 @@
+package User_Input;
+
+public class Detail {
+    public static void main(String[] args) {
+        String age = args[0];
+        String Name = args[1];
+        String Salary = args[2];
+        int Age = Integer.parseInt(age);
+        int salary = Integer.parseInt(Salary);
+        System.out.println(age);
+        System.out.println(Name);
+        System.out.println(Salary);
+
+
+    }
+}
