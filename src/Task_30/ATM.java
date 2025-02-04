eaa2af3f3c82f8614835e3e30513b3ff0e1688f4
Sheetal-jagotra <sheetal95jagotra@gmail.com>
2025-02-04 21:43:23 +0530

    ATM

---
+++ src/Task_30/ATM.java
@@ -0,0 +1,29 @@
+package Task_30;
+import java.util.Scanner;
+public class ATM {
+    public static void main(String[] args) {
+    Withdraw();
+    }
+    static void Withdraw(){
+        int Acc_Balance= 10000;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the you want to withdraw ");
+        int amount = obj.nextInt();
+
+        if (amount==0){
+            System.out.println("Amount should be greater than 0 ");
+        }
+        else if (amount%100 != 0){
+                System.out.println("Amount should be greater than 0 and multiple of 100");
+        }
+        else if (amount <= Acc_Balance){
+            int c = Acc_Balance-amount;
+            System.out.println("Remaining Balance in the account "+ c);
+        }
+
+
+
+
+    }
+}
+
