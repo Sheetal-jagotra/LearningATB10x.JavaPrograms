6f3cd5194a26a17b813bc7f3372c9777caec6e3c
Sheetal-jagotra <sheetal95jagotra@gmail.com>
2025-02-05 23:17:23 +0530

    "Palindrome_reverse"

---
+++ src/Task_31/Palindrome.java
@@ -0,0 +1,16 @@
+package Task_31;
+
+import java.util.Scanner;
+
+import static java.util.Collections.reverse;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String ch = obj.nextLine();
+        StringBuilder a = new StringBuilder(ch);
+        a.reverse();
+        System.out.println(a);
+    }
+}
